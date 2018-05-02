a403aa2dd78f0d2c9acdd57a5c14ae2b2127a82e
Joe Warren <joe.warren7@gmail.com>
2018-05-02 10:53:56 -0500

    style: add icon to appbar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,6 +4,8 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import Loop from '@material-ui/icons/Loop';
 
 const styles = {
     root: {
@@ -22,8 +24,9 @@ function SimpleAppBar(props) {
         <div className={classes.root}>
             <AppBar position="fixed" color="default" style={customStyles}>
                 <Toolbar>
+                        <Loop style={{marginRight: 5}}/>
                     <Typography variant="title" color="inherit">
-                        The Redux Poster
+                        the-redux-poster
           </Typography>
                 </Toolbar>
             </AppBar>
